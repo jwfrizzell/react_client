88b7d613c658998adda18fe55797ca3d45290866
Joe Frizzell <joefrizzell@Joes-MacBook-Pro-2.local>
2018-08-13 16:30:44 -0400

    Wire up redux actions and reducers. Authenticate User

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,18 +1,53 @@
 import React, { Component } from "react";
+import { compose } from "redux";
+import { reduxForm, Field } from "redux-form";
+import { connect } from "react-redux";
+import * as actions from "actions";
 
 class Signup extends Component {
+	onSubmit = formProps => {
+		this.props.signup(formProps, () => {
+			this.props.history.push("/feature");
+		});
+	};
+
 	render() {
+		const { handleSubmit } = this.props;
 		return (
-			<form>
+			<form onSubmit={handleSubmit(this.onSubmit)}>
 				<fieldset>
 					<label>Email</label>
+					<Field
+						name="email"
+						type="text"
+						component="input"
+						autoComplete="off"
+					/>
 				</fieldset>
 				<fieldset>
 					<label>Password</label>
+					<Field
+						name="password"
+						type="password"
+						component="input"
+						autoComplete="off"
+					/>
 				</fieldset>
+				<div>{this.props.errorMessage}</div>
+				<button>Signup</button>
 			</form>
 		);
 	}
 }
 
-export default Signup;
+function mapStateToProps(state) {
+	return { errorMessage: state.auth.errorMessage };
+}
+
+export default compose(
+	connect(
+		mapStateToProps,
+		actions
+	),
+	reduxForm({ form: "signup" })
+)(Signup);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import reduxThunk from "redux-thunk";
+
 import App from "components/App.js";
 import Welcome from "components/Welcome.js";
 import Signup from "components/auth/Signup.js";
+import reducers from "reducers";
+
+const store = createStore(
+	reducers,
+	{}, //Initial state
+	applyMiddleware(reduxThunk)
+);
 
 ReactDOM.render(
-	<Router>
-		<App>
-			<Route exact path="/" component={Welcome} />
-			<Route exact path="/signup" component={Signup} />
-		</App>
-	</Router>,
+	<Provider store={store}>
+		<Router>
+			<App>
+				<Route exact path="/" component={Welcome} />
+				<Route exact path="/signup" component={Signup} />
+			</App>
+		</Router>
+	</Provider>,
 	document.querySelector("#root")
 );
